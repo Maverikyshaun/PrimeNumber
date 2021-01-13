76e4e90820c2da5488f0c0379fa993862ec9ffad
disssid <siddharthsoni993@gmail.com>
2021-01-13 23:22:33 +0530

    Refactor prime function

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-def prime_or_comp(user_input: int) -> str:
-    if user_input == 0 or user_input == 1:
-        return "neither prime nor composite"
-
-    if user_input == 2:
-        return "prime"
-
-    else:
-        for i in range(2, user_input):
-            if user_input % i == 0:
-                return "composite"
-        return "prime"
+def is_prime(user_input: int) -> bool:
+    for i in range(0, user_input):
+        if user_input == 0 or user_input == 1 or user_input % i == 0:
+            return False
+        return True
 
 
 x = int(input())
-y = prime_or_comp(x)
+y = is_prime(x)
 
 
